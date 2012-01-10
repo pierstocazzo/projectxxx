20dcee35865f33b178d154a7c539fd042df07f2a
sepp.hoedenaeken@gmail.com <sepp.hoedenaeken@gmail.com@9e0affc4-be4a-d98a-f904-4194842b9de0>
2012-01-10 09:26:20 +0000

    testje

--- trunk/ProjectX/src/chatbox/Test.java
+++ trunk/ProjectX/src/chatbox/Test.java
@@ -1,5 +1,11 @@
 package chatbox;
 
 public class Test {
-	http://www.youtube.com/watch?v=dQw4w9WgXcQ
+	/* 
+	 * http://www.youtube.com/watch?v=dQw4w9WgXcQ
+	*/
+	
+	public void test() {
+		//do something
+	}
 }
